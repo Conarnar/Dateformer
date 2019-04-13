ff8bfa191ea776b51d95c4ac9fba8fb7d6a10b67
Conan Truong <truong.conan@gmail.com>
2019-04-13 13:35:48 -0700

    Bullet enemies

---
+++ Dateformer/Assets/Scripts/Platforming/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (pos.x < -0.1f || pos.x > 1.1f || pos.y < -0.1f || pos.y > 1.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
