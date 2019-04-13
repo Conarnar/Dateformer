9727e76d24b5d266571937f018077de5493c2170
Evan Cheng <dr.frolicks@gmail.com>
2019-04-13 13:27:42 -0700

    spike prototype and kill player script prototype

---
+++ Dateformer/Assets/Scripts/Platforming/ColliderKillsPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Kills the player when the player touches the collider. 
+/// last edited: Evan Cheng (4/13/19)
+/// </summary>
+public class ColliderKillsPlayer : MonoBehaviour
+{
+    public string enemyName;  
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player died to " + enemyName); 
+        }
+    }
+}
