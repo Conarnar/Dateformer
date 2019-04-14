2f40c068adde90f29be1a7faa5c72d58a2eaa4bc
Evan Cheng <dr.frolicks@gmail.com>
2019-04-13 20:52:28 -0700

    turtle bugfixes

--- Dateformer/Assets/Scripts/Platforming/ColliderBouncesPlayer.cs
+++ Dateformer/Assets/Scripts/Platforming/ColliderBouncesPlayer.cs
@@ -15,5 +15,14 @@ public class ColliderBouncesPlayer : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, bounceVelocity);  
         }
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("collided");
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(rb.velocity.x, bounceVelocity);
+        }
+    }
 }
