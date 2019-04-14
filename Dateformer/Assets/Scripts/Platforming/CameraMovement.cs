f392980b8b2387099dfb3c79024ee8849b20b060
Conan Truong <truong.conan@gmail.com>
2019-04-13 18:54:30 -0700

    Camera movement script

---
+++ Dateformer/Assets/Scripts/Platforming/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Vector2 bottomLeftBounds;
+    [SerializeField] Vector2 topRightBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, bottomLeftBounds.x, topRightBounds.x), Mathf.Clamp(player.position.y, bottomLeftBounds.y, topRightBounds.y), transform.position.z);
+    }
+}
